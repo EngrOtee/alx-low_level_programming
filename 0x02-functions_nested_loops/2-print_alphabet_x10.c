26a7fa7e85e79799a74cdacabf0569ab43a650c3
Otobong <oeumoh@gmail.com>
2022-09-16 09:33:02 -0700

    initial commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,13 +1,13 @@
 /*
- *  * File: 2-print_alphabet_x10.c
- *   * Auth: Oto-Obong Ezekiel Umoh
+ * File: 2-print_alphabet_x10.c
+ * Auth: Oto-Obong Ezekiel Umoh
  */
 
 #include "main.h"
 
 /**
- *  * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase.
- *   */
+ * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase.
+ */
 void print_alphabet_x10(void)
 {
 		int count = 0;
