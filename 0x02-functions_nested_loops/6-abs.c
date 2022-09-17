dc9d54fdf27414c1b5c4c937d2a3b74b74ae2257
Otobong <oeumoh@gmail.com>
2022-09-17 07:16:34 -0700

    initial commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,11 @@
+#inlcude "main.h"
+/**
+ *  _abs - function that computes the absolute value of an integer.
+ *  @ab: input letter
+ *  Return: Always success
+ */
+int _abs(int ab)
+{
+return (ab * ((ab > )) - (ab < 0))
+	;
+}
