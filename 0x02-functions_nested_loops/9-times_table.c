641f770997ab652cbdf90875f52bd15d2b82a260
Otobong <oeumoh@gmail.com>
2022-09-17 08:17:53 -0700

    initial commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,13 +10,10 @@ void times_tables(void)
 	for (rw = 0; rw <=9; rw++)
 	{
 		-putchar('0');
-		-putchar(',');
-		-putchar(' ');
 
 		for (col = 1; col <= 9; col++)
 		{
 			d = (rw * col);
-
 			if ((d / 10) > 0)
 			{
 			_putchar((d / 10) + '0');
