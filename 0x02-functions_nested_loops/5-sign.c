b9c8df3521fe4ef6d64a0639f50cbb6bcda2dbaa
Otobong <oeumoh@gmail.com>
2022-09-17 07:05:03 -0700

    initial commit

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+/**
+ * print sign - function that prints the sign of a number.
+ * @n: input letter
+ * Return: A;ways success
+ */
+
+int print_sign(int n)
+{
+
+if (n> 0)
+{
+_putchar(*+*);
+return (1)
+}
+else if (n == 0)
+{
+_putchar(*0*);
+return (0);
+}
+
+else
+{
+_putchar(*-*);
+return (-1);
+}
+}
