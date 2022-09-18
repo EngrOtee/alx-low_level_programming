46e58a6261ff6ee1e03eb57b1b9c2ab0db6045c1
Otobong <oeumoh@gmail.com>
2022-09-18 06:28:44 -0700

    intial commit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,17 +1,25 @@
 #include "main.h"
+
 /**
- * jack_bauer - function that prints every minute of the day of Jac Bauer
- * h = hour, m = minutes
- * Return: Always success
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ *
+ * Return: Always success.
  */
+
 void jack_bauer(void)
 {
-	int h, m;
-	for (h = 0; h < 24; h++)
+	int hr;
+	int min;
+	for (hr = 0; hr < 24; hr++)
 	{
-		for (m = 0; m < 60; m++)
+		for (min = 0; min < 60; min++)
 		{
-			_putchar((h / 60) + '0');
+			_putchar((hr / 10) + '0');
+			_putchar((hr % 10) + '0');
+			_putchar(':');
+			_putchar((min / 10) + '0');
+			_putchar((min % 10) + '0');
+			_putchar('\n');
 		}
 	}
 }
