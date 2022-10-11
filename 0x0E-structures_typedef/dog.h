5bc51f284b2948270839803ce392e63324df3b51
Otobong <oeumoh@gmail.com>
2022-10-11 05:30:07 -0700

    structures

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,12 +6,9 @@
  * @d: A dog structure.
  * @name: The name of the dog.
  * @age: The age of the dog.
- *
  * @owner: The owner of the dog.
- *
  * Return: Nothing
  */
-
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 		if (d == NULL)
